4b5d237655f8a4cf622cd59782a055e3d74e4b3f
DeboAdeniran <158854537+DeboAdeniran@users.noreply.github.com>
2024-12-23 22:15:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const navLinks = document.querySelectorAll(".nav-link");
 
 navLinks.forEach((link) => {
@@ -7,3 +8,14 @@ navLinks.forEach((link) => {
     link.classList.add("active");
   });
 });
+=======
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    console.log("Link clicked:"); // Debug log
+    navLinks.forEach((nav) => nav.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+>>>>>>> 4124556493c7797cd7527e688da86523594197d1
